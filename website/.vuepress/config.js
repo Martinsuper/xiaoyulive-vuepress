7f32e43095c45f70c4ed4337b92bb54b10e89434
Quanzaiyu <731734107@qq.com>
2018-05-21 01:36:24 +0800

    0

--- website/.vuepress/config.js
+++ website/.vuepress/config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
+  dest: 'disti',
   configureWebpack: {
     resolve: {
       alias: {
